5a3ae77668acb12c6eb724aead848a38c9131692
Luiz Henrique <luizhsc.araujo@outlook.com>
2021-01-23 09:22:41 -0300

    feat: adding the server version and the cors

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 const express = require('express');
 require('dotenv/config');
-const routes = require('./routes/routes.index');
+const routes = require('./routes/index.routing');
+const cors = require('cors');
+fs = require('fs')
+const version = fs.readFileSync('./version','utf8');
 
 const app = express();
 const port = process.env.SERVER_PORT || 3000;
 
+//Rotas dos endpoints
 app.use(routes);
-
+//Cors
+app.use(cors());
 
 
 
 app.listen(port, () => {
-    console.log(`Listening on the port: ${port}`);
-
+    console.log(`
+    Listening on the port: ${port}
+    Server release:  ${version}`
+    );
 });
